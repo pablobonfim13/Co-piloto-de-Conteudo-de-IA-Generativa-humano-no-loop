9be84d86c928645bc1d9a17ee59ab60efc846b3f
pablobonfim13 <pabloevertonsousa@gmail.com>
2025-12-18 15:22:31 -0300

    Update verificar_modelos.py

--- verificar_modelos.py
+++ verificar_modelos.py
@@ -2,7 +2,7 @@
 import google.generativeai as genai
 
 # COLOQUE A MESMA CHAVE DE API QUE VOCÊ ESTÁ USANDO NO OUTRO SCRIPT
-SUA_API_KEY = "AIzaSyDn1Cvb9UP-5xEQs_Hn1K5UZBBQTad75EY"
+SUA_API_KEY = ""
 genai.configure(api_key=SUA_API_KEY)
 
 print("--- Modelos de IA que sua chave de API pode acessar ---")
